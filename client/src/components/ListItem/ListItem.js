af0734ee337a2bc5735a2812ef54f4a3cc6b577a
MadiGabrielle <marianafinamori@Marianas-MBP-2.lan1>
2019-06-13 00:40:05 -0400

    Started styling list results || Changed navbar to a responsive height

--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -1,33 +1,3 @@
-// import React, {Component} from 'react';
-// import './ListItem.css'
-
-// export class ListItem extends Component {
-//     render() {
-//         // const {id, title, authors, description, image, link} = this.props.book;
-//         return (
-//             <div key={this.props.book.id.toLowerCase()}>
-//                 <h3>{ this.props.book.volumeInfo.title}</h3>
-//                 <div>
-//                     <img src={this.props.book.volumeInfo.imageLinks.smallThumbnail} alt="book cover" style={{height: "100px"}}></img>
-//                 </div>
-//                 <div> 
-//                     {this.props.book.volumeInfo.authors.map(author => (
-//                         <p>{ author }</p>
-//                     ))}
-//                 </div>
-//                     <p>{ this.props.book.volumeInfo.description }</p>
-//                     <a rel="noreferrer noopener" target="_blank" href={ this.props.book.accessInfo.webReaderLink }>
-//                         <button className="btn btn-dark">Go to Google Books</button>
-//                     </a>
-//                     <button id = { this.props.book.id.toLowerCase() } className = "btn btn-dark save-btn" onClick={this.props.handleSaveButton.bind(this, this.props.book.id)}>Save book </button>
-//             </div>
-//         )
-//     }
-
-// }
-
-// export default ListItem;
-
 import React, {Component} from 'react';
 import './ListItem.css'
 
