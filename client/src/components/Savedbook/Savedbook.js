22ccb4d7f1cb9f677b8c6a435b79a8d56784464c
MadiGabrielle <marianafinamori@Marianas-MBP-2.lan1>
2019-06-14 08:07:26 -0400

    styled saved books page

--- client/src/components/Savedbook/Savedbook.js
+++ client/src/components/Savedbook/Savedbook.js
@@ -5,25 +5,34 @@ export class Savedbook extends Component {
     render() {
         const {_id, title, authors, description, image, link} = this.props.savedbook;
         return (
-            <div key={_id}>
-    
-                                <h3>{title}</h3>
-                                <div>
-                                     <img src={image} alt="book cover" style={{height: "100px"}}></img>
-                                </div>
-                                <div> 
-                                {authors.map(author => (
-                                    <p>{ author }</p>
-                                ))}
-                                </div>
-                                 
-                                 <p>{ description }</p>
-                                 <a rel="noreferrer noopener" target="_blank" href={ link }>
-                                     <button className="btn btn-dark">Go to Google Books</button>
-                                 </a>
-                                 <button id = {_id} className = "btn btn-dark save-btn" onClick={this.props.delBook.bind(this, _id)}>Delete</button>
+            <div className="row justify-content-aroud" key={_id}>
+                <div className="col-lg-4 book-left-saved d-flex align-content-center flex-wrap"> 
+                    <div className="divCoverName">
+                        <div>
+                            <img src={image} alt="book cover"></img>
+                        </div>
+                        <h5 className="title">{title}</h5>
+                    </div>
+                </div>
+                <div className="col-lg-7 divSummary"> 
+                    <h3>{ title }</h3>
+                    <p>{ description }</p>
+                    <span>Author(s): </span>
+                    { authors.map(author => (
+                        <React.Fragment>
+                            <span>{ author }</span>{ " " }
+                        </React.Fragment>
+                    ))}
                 
-                 </div>
+                <div>
+                    <a rel="noreferrer noopener" target="_blank" href={ link }>
+                        <button className="googleBtn">Go to Google Books</button>
+                    </a>
+                    <button id = {_id} className = "deleteBtn" onClick={this.props.delBook.bind(this, _id)}>Delete</button>     
+                </div>
+                </div>
+                
+            </div>
         )
     }
 
